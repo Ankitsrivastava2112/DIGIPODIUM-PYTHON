11380db0c8782e9e2244173a822c1b7da7a06e67
Ankit srivastava <ankitsrivastava21122001@gmail.com>
2023-02-14 20:01:46 +0530

    nested loop

---
+++ nested_loop.py
@@ -0,0 +1,21 @@
+from turtle import*
+speed(0)
+pencolor('blue')
+fillcolor('yellow')
+pensize(5)
+side = 4
+for i in range(side):
+    fd(150)
+    begin_fill()
+    for i in range(side):
+        fd(75)
+        for i in range(side):
+            fd(75/2)
+            lt(360/side)
+            dot(25)
+        rt(360/side)        
+    end_fill()
+    lt(360/side)    
+
+done()
+hideturtle()    
