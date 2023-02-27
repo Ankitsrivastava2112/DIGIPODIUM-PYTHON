d07fbf4b4eb5e9b68eb5cedfb1d0bc004d5d8f07
Ankit srivastava <ankitsrivastava21122001@gmail.com>
2023-02-27 19:46:32 +0530

    list part 1

--- game1.py
+++ game1.py
@@ -1,37 +1,49 @@
 import pgzrun
-box = Rect((50,50),(150,100))
-box2 = Rect((600,50),(150,100))
-box3 = Rect((300,50),(150,100))
-box4 = Rect((300,200),(150,100))
-bvx = 5
-b2vx = -5
-b3vx = 5
-b4vx = -5
+WIDTH = 1500
+HEIGHT = 800
+box1 = Rect((50,250),(150,100))
+box2 = Rect((650,250),(150,100))
+box3 = Rect((350,0),(150,100))
+box4 = Rect((350,500),(150,100))
+bvx = 2
+b2vx = -2
+b3vx = 1
+b4vx = 1
 
 def draw():
     screen.fill('black')
-    screen.draw.filled_rect(box,'yellow')
-    screen.draw.filled_rect(box2,'Purple')
+    screen.draw.filled_rect(box1,'yellow')
+    screen.draw.filled_rect(box2,'blue')
     screen.draw.filled_rect(box3,'Red')
-    screen.draw.filled_rect(box4,'Green')
+    screen.draw.filled_rect(box4,'white')
 
 def update():
     global bvx,b2vx,b3vx,b4vx
+    box1.x += bvx
+    box2.x += b2vx
     box3.y += b3vx
     box4.y += b4vx
-    box.x += bvx
-    box2.x += b2vx
 
-    if box3.colliderect(box4):
-        b3vx = -b3vx
-        b4vx = -b4vx
-
-    if box.colliderect(box2):
+    if box1.colliderect(box2):
         bvx = -bvx
         b2vx = -b2vx
         sounds.explosion1.play()
 
-    if box3.top < -600:
+    if box3.colliderect(box4):
+        b3vx = -b3vx
+        b4vx = -b4vx
+        sounds.cling.play()
+
+
+    if box1.left < 0:
+        bvx = -bvx
+        sounds.explosion1.play()
+
+    if box2.right > 850:
+        b2vx = -b2vx
+        sounds.explosion1.play()    
+
+    if box3.top < 0:
         b3vx = -b3vx
         sounds.cling.play()
 
@@ -39,13 +51,5 @@ def update():
         b4vx = -b4vx
         sounds.cling.play()
 
-    if box.left <0:
-        bvx = -bvx
-        sounds.explosion1.play()
-
-    if box2.right > 800:
-        b2vx = -b2vx
-        sounds.explosion1.play()
-
-
+    
 pgzrun.go()
